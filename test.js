b4d951479b8b12145afbcc6408d8b5366af02404
vaclavlangr <vendavenda3@gmail.com>
2017-08-17 11:55:41 +0200

    Added different generator functions. Corrected preProcess function. Next: Compare MFCC values with values computed via Meyda package. (After normalization negative values remain).

--- test.js
+++ test.js
@@ -1,20 +1,28 @@
 var extractor = require('sound-parameters-extractor');
 
-function preProcess(data) {
-    var noiseCoefs = [-1, 1]; //Should be only [-1, 1] at random or any number between <-1, 1>?
+function generateRandom() {
+    return (Math.random()*2 - 1);
+}
 
-    var filtered = Array.from(data);
-    var backup1 = filtered[0];
-    var backup2;
-    for(var i = 1; i < filtered.length; i++) {
-        backup2 = filtered[i];
-        filtered[i] = filtered[i] - 0.97*backup1;
-        backup1 = backup2;
+function generateRandomFromCoefs() {
+    return this.coefs[Math.floor(Math.random() * this.coefs.length)];
+}
+
+function preProcess(data, noiseCoefs) {
+    var generator;
+    if (typeof noiseCoefs === 'undefined') {
+        generator = generateRandom;
+    } else {
+        generator = generateRandomFromCoefs.bind({coefs:noiseCoefs});
     }
 
-    var signal = Array.from(data).map((val, index) =>
-        val + noiseCoefs[Math.floor(Math.random() * noiseCoefs.length)]);
+    var filtered = new Array(data.length);
+    filtered[0] = data[0];
+    for(var i = 1; i < filtered.length; i++) {
+        filtered[i] = data[i] - 0.97*data[i-1];
+    }
 
+    var signal = filtered.map((val, index) => val + generator());
     return signal;
 }
 
@@ -22,7 +30,7 @@ function divideIntoFrames(signal) {
     const windowSize = 400;
     const overlap = '40%';
     frames = extractor.framer(signal, windowSize, overlap);
-    frames = frames.slice(0,-3); //Should throw away last 3 frames (mostly zeros or very little part of audio signal)?
+    frames = frames.slice(0,-3);
     return frames;
 }
 
@@ -74,7 +82,7 @@ function normalize(mfccFeatures, left, right) {
 }
 
 function extractFeatures(data) {
-    var preProcessedSignal = preProcess(data);
+    var preProcessedSignal = preProcess(data, [-1, 1]);
     var framedSignal = divideIntoFrames(preProcessedSignal);
     var mfccFeatures = computeMfcc(framedSignal);
     var normalizedFrames = normalize(mfccFeatures, 50, 50);
@@ -86,7 +94,7 @@ function onChange() {
     fr.onload = function () {
         var wav = require('node-wav');
         var result = wav.decode(this.result);
-
+        
         if(result.sampleRate != 16000) {
             //Gets different values compared to MATLAB resample
             resampler = require('audio-resampler');
