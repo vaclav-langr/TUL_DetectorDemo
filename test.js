ab0914ce7c070f705f0f38b394b0f62ec452dd4a
vaclavlangr <vendavenda3@gmail.com>
2017-08-21 13:21:38 +0200

    Edited behavior of data processing to match stream processing. So data is divided into frames of length 160 (cca length of stream buffer), adding random noise to this frame (so the noise is the same in next processing) and filtering using last unaltered sample of the last frame. Then this frame is push into buffer (first 160 samples are shifted away) and as a result new window of length 400 is received. Next: Find faster FFT library.

--- test.js
+++ test.js
@@ -1,29 +1,36 @@
 var library = require('./library');
 var config = require('./config').config;
-var extractor = require('sound-parameters-extractor');
-
-function divideIntoFrames(signal) {
-    frames = extractor.framer(signal, config.windowSize, config.overlapPercent);
-    frames = frames.slice(0,-3);
-    return frames;
-}
 
 function extractFeatures(data) {
     var empty = new Array(config.mfccCount).fill(0);
-    var framedSignal = divideIntoFrames(Array.from(data));
+    var padding = new Array(config.overlap - (data.length % config.overlap)).fill(0);
+    var paddedSignal = Array.from(data).concat(padding);
+    var buffer = new Array(config.windowSize).fill(0);
+    var lastSample = 0;
+    var frame, normalized, preProcessedSignal, mfccFeatures;
 
-    for(var i = 0; i < framedSignal.length; i++) {
-        preProcessedSignal = library.preProcess(framedSignal[i],
-            (i > 0) ? framedSignal[i-1][framedSignal[i-1].length - 1] : 0,
-            config.noiseCoefs);
-        mfccFeatures = library.computeMfcc(preProcessedSignal);
-        var normalized = library.normalize(mfccFeatures);
+    for(var i = 0; i < paddedSignal.length / config.overlap; i++) {
+        frame = paddedSignal.slice(i*config.overlap, (i+1)*config.overlap);
+
+        preProcessedSignal = library.preProcess(frame, lastSample, config.noiseCoefs);
+        lastSample = frame[frame.length - 1];
+
+        for(var j = 0; j < config.overlap; j++) {
+            buffer[j] = buffer[j + config.overlap];
+        }
+        for(var j = 0; j < config.overlap; j++) {
+            buffer[j + config.windowSize - config.overlap] = preProcessedSignal[j];
+        }
+
+        mfccFeatures = library.computeMfcc(buffer);
+
+        normalized = library.normalize(mfccFeatures);
         if(typeof normalized !== 'undefined') {
             console.log(normalized);
         }
     }
     for(var i = 0; i < config.right; i++) {
-        var normalized = library.normalize(empty);
+        normalized = library.normalize(empty);
         console.log(normalized);
     }
 }
